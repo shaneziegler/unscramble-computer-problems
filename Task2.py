3be9c7138c489474c1220c66e8489dbb0d725381
Shane-Ziegler <shanedb@gmail.com>
2021-03-01 22:46:56 -0600

    Got rid of texts file read

--- Task2.py
+++ Task2.py
@@ -3,9 +3,6 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
